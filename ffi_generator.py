34401a0f4bd3b17d0f094d0fadbb804e5d0bf3e5
Stanislav Dimov <stanislav.vdimov@gmail.com>
2024-05-27 21:33:54 +0100

    Include `ITC_*` preprocessor definitions in the generated pyext code

--- ffi_generator.py
+++ ffi_generator.py
@@ -4,20 +4,36 @@
 # license. Refer to the LICENSE file for details or visit:
 # https://www.gnu.org/licenses/agpl-3.0.en.html
 
+import re
 import sys
 
 import cffi
 
-if len(sys.argv) != 3:
-    raise RuntimeError("Requires two arguments")
+if len(sys.argv) != 4:
+    raise RuntimeError("Requires three arguments")
 
 header_file = sys.argv[1]
-module_name = sys.argv[2]
+header_definitions_file = sys.argv[2]
+module_name = sys.argv[3]
 
 ffibuilder = cffi.FFI()
 
 with open(header_file) as f:
     ffibuilder.cdef(f.read())
+
+with open(header_definitions_file) as f:
+    contents = f.read()
+    # Sanitize the output due to `pycparser` limitations
+    # Remove defines not starting with `ITC_`
+    contents = re.sub(r"^(?!#define\s+ITC).*\n", '', contents, flags=re.MULTILINE)
+    # Remove defines without value (i.e. `#define SOMETHING`)
+    contents = re.sub(r"^#define\s+\w+\s*\n", '', contents, flags=re.MULTILINE)
+    # Remove suffixes from integer literals (`1U` -> `1`, `0xAFuLL` -> `0xAF`)
+    contents = re.sub(r"(\b(?:0[xXbB])?[\da-fA-F]+)[UulL]+\b", r'\1', contents, flags=re.MULTILINE)
+    # Remove brackets
+    contents = re.sub(r"[\(\)]", '', contents, flags=re.MULTILINE)
+
+    ffibuilder.cdef(contents)
 
 ffibuilder.set_source(
     module_name,
