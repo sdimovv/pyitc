229fc794b035cf0eceb1282cac587680648f28d5
Stanislav Dimov <stanislav.vdimov@gmail.com>
2024-06-12 01:16:40 +0100

    Add serailise-to-string feature to the supported features map

--- pyitc/__init__.py
+++ pyitc/__init__.py
@@ -15,5 +15,6 @@ __all__ = [
 SUPPORTED_FEATURES: Dict[str, bool] = MappingProxyType({
     'EXTENDED_API': bool(_lib.ITC_CONFIG_ENABLE_EXTENDED_API),
     '64_BIT_EVENT_COUNTERS': bool(_lib.ITC_CONFIG_USE_64BIT_EVENT_COUNTERS),
+    'SERIALISE_TO_STRING_API': bool(_lib.ITC_CONFIG_ENABLE_SERIALISE_TO_STRING_API),
 })
 """A mapping describing the supported features by the underlying C library"""
