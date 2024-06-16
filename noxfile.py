f85807f3c83ceed41f528c584b586f9b4d3166b4
Stanislav Dimov <stanislav.vdimov@gmail.com>
2024-06-16 14:51:04 +0100

    Fix python 3.7 and 3.12 build support

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,10 @@
 import nox
 
-@nox.session
+SUPPORTED_PYTHON_VERSIONS = ["3.7", "3.8", "3.9", "3.10", "3.11", "3.12"]
+
+@nox.session(python=SUPPORTED_PYTHON_VERSIONS)
 def test(session: nox.Session) -> None:
     """Run the test suite."""
+    session.run('python', '-m', 'ensurepip', '--upgrade')
     session.install('.[test]')
     session.run('pytest', '--cov=pyitc', *session.posargs)
