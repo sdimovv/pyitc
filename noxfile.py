3c46a34642829c83ffc2bd02da865b301d27da58
Stanislav Dimov <stanislav.vdimov@gmail.com>
2024-06-16 16:03:10 +0100

    Run tests on CI

--- noxfile.py
+++ noxfile.py
@@ -1,10 +1,18 @@
 import nox
+import re
 
-SUPPORTED_PYTHON_VERSIONS = ("3.7", "3.8", "3.9", "3.10", "3.11", "3.12")
+def _natural_sort(l):
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    return sorted(l,
+        key=lambda key: [convert(c) for c in re.split('([0-9]+)', key)])
+
+with open('.python-version', 'r') as f:
+    SUPPORTED_PYTHON_VERSIONS = _natural_sort(
+        re.sub(r'[\n\s]+', ' ', f.read()).split())
 
 @nox.session(python=SUPPORTED_PYTHON_VERSIONS)
 def test(session: nox.Session) -> None:
-    """Run the test suite."""
+    """Run the tests."""
     session.run('python', '-m', 'ensurepip', '--upgrade')
     session.install('.[test]')
     session.run('pytest', '--cov=pyitc', *session.posargs)
